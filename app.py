096c85e474d1e1183af67de1311641320ea1d51d
Ashwani Kumar <ashwanikumar200508@gmail.com>
2025-06-04 01:39:15 +0530

    new app build

--- app.py
+++ app.py
@@ -30,9 +30,12 @@ def login():
     session['regno'] = reg_no
 
     options = Options()
-    # options.add_argument('--headless')
+    #options.add_argument('--headless')
+    options.add_argument('--headless')  # enable headless mode (mandatory in server)
     options.add_argument('--disable-gpu')
-    options.add_argument("--no-sandbox")
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-dev-shm-usage')  # fixes issues in small containers
+    options.add_argument('--remote-debugging-port=9222')
 
     driver = webdriver.Chrome(options=options)
     driver.get("https://www.srmimthostel.net/olms")
